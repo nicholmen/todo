772d35e4e2fba047c39eb7b20fafa973f815e23a
Thomas Nicholson <nicholmen@gmail.com>
2018-01-17 12:36:33 -0800

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+import SingleTodo from "./SingleTodo";
+
 class App extends Component {
-  render() {
+constructor() {
+  super();
+  this.state = {
+    todos: [],
+    currentTodo: ""
+  };
+}
+
+onInputChange = event => {
+  this.setState({ currentTodo: event.target.value })
+}
+
+onClick = () => {
+  let todosCopy = this.state.todos.slice();
+  todosCopy.push(this.state.currentTodo);
+
+  this.setState({ todos: todosCopy, currentTodo: "" });
+}
+
+deleteTodo = i => {
+  let todosCopy = this.state.todos.slice();
+
+  todosCopy.splice(i, 1);
+
+  this.setState({ todos: todosCopy });
+}
+
+render() {
+  let bulletedTodos = this.state.todos.map((element, index) => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+      <SingleTodo todo={element} delete={() => this.deleteTodo(index)} />
+    )
+  })
+  return (
+    <div>
+      <input placeholder="Enter todo" value ={this.state.currentTodo}
+        onChange={this.onInputChange} />
+      <button onClick={this.onClick}>Add</button>
+      <br />
+      { this.state.todos.length === 0 ? "No todos yet" : <ul>{bulletedTodos}</ul> }
+    </div>
+  )
+}
 }
 
 export default App;
---
+++ src/SingleTodo.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+// class SingleTodo extends Component {
+//   constructor() {
+//     super();
+
+//   }
+
+//   render() {
+//     return (
+//       <li>{this.props.todo}<button onClick={this.props.delete}>X</button></li>
+//     );
+//   }
+// }
+
+// this component has no internal state, everything about it is determined by what the parent says it should be. Has no internal state
+// instead of creating a class, create a functional component. Only accepts props
+const SingleTodo = props => {
+  return (
+    <li>{props.todo}<button onClick={props.delete}>X</button></li>    
+  );
+}
+
+export default SingleTodo;
